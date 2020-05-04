e8d445c5a1f04ded9fee84cb1c5e555947e4a15d
Nick McCarthy <nik.mccarthy@gmail.com>
2020-05-04 12:42:21 +1000

    Revised to meet project rubric requirements

--- designs.js
+++ designs.js
@@ -1,28 +1,28 @@
-// define variables
-var form = document.getElementById("sizePicker");
-var grid = document.getElementById("pixelCanvas");
+// define constiables
+const form = document.getElementById('sizePicker');
+const grid = document.getElementById('pixelCanvas');
 
 // function to create user defined grid
 function makeGrid(height, width) {
   // loop over each row
-  for (var y = 0; y < height; y++) {
-    var row = grid.insertRow(y);
+  for (let y = 0; y < height; y++) {
+    const row = grid.insertRow(y);
     // loop over each cell
-    for (var x = 0; x < width; x++) {
-      var cell = row.insertCell(x);
+    for (let x = 0; x < width; x++) {
+      const cell = row.insertCell(x);
       // add event listener to define colour
-      cell.addEventListener("click", function(e){
-        var colour = document.getElementById("colorPicker").value;
+      cell.addEventListener('click', function(e){
+        const colour = document.getElementById('colorPicker').value;
         e.target.style.backgroundColor = colour;
       });
     }
   }
 }
 // When size is submitted by the user, call makeGrid()
-form.addEventListener("submit", function(event) {
-  var height = document.getElementById("inputHeight").value;
-  var width = document.getElementById("inputWidth").value;
+form.addEventListener('submit', function(event) {
+  const height = document.getElementById('inputHeight').value;
+  const width = document.getElementById('inputWidth').value;
   event.preventDefault(); //prevent submitting form to server
-  grid.innerHTML = ""; // empty table
+  grid.innerHTML = ''; // empty table
   makeGrid(height, width);
 });
